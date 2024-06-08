f4e41c27f5263f2660cf6542cb8b5a0e4ed4730c
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-06-08 01:05:08 -0400

    created components

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import { Section } from '../Section'
+import logo from '../../assets/logo.svg'
+export const Header = () => {
+  return (
+    <header>
+        <div><img src={logo}/></div>
+        <Section />
+
+    </header>
+  )
+}
---
+++ src/components/Section.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const Section = () => {
+  return (
+    <section>
+      <div>
+        <img />
+      </div>
+      <div>
+        <img />
+      </div>
+      <div>
+        <h2>
+          Get paid for the work you <span>love</span> to do.
+        </h2>
+        <p>
+          The 9-5 grind is so last century. We believe in living life on your
+          own terms. Whether you’re looking to escape the rat race or set up a
+          side hustle, we’ve got you covered.
+        </p>
+      </div>
+    </section>
+  );
+};
